7ce6d15e9abfd9a2f40e4c3d3a45cf7fcac17cfb
Adam Jędrzejewski <adamjedrzejewski@icloud.com>
2021-12-01 18:16:45 +0100

    empty project

---
+++ Controllers/CountryListController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryList.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class CountryListController : ControllerBase
+    {
+        [HttpGet("{countryCode}")]
+        public async Task<IActionResult> GetCountryListAsync(string countryCode)
+        {
+            return Ok(countryCode);
+        }
+    }
+}
